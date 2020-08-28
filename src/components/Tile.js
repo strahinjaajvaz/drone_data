88a25f40213952b792398b7c1c118a189cfcc57b
Strahinja Ajvaz <strahinja.ajvaz@gmail.com>
2020-08-28 23:22:28 +1000

    fixed position of tracker node

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -11,12 +11,12 @@ function Tile({ visited, current }) {
 
     return <div
         {...{ ref }}
-        className={`h-6 w-6 border-2 rad-6 border-purple-900 text-center rounded m-1 inline-block box-border relative
+        className={`h-6 w-6 border-2 rad-6 border-purple-900 text-center rounded m-1 inline-block box-border
             ${visited
                 ? "bg-blue-400"
                 : visited === undefined ? "" : "bg-green-400"} `}>
         {visited || ""}
-        {current && <span className="absolute z-10 t" >X</span>}
+        {current && <span className="relative z-10 text-red-700 text-3xl inset-0" style={{ top: -13 }}>X</span>}
     </div>
 }
 
