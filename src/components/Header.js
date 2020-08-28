243584653ee2e42fba923a8399bd767337adcb57
Strahinja Ajvaz <strahinjaajvaz@Strahinjas-MBP.modem>
2020-08-28 18:59:13 +1000

    added routing and started boilerplate

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Wrapper from './Wrapper'
+
+function Header({ }) {
+    return <div class="bg-gray-400">
+        <Wrapper>
+            <nav>Home</nav>
+        </Wrapper>
+    </div>
+}
+
+export default Header;
---
+++ src/components/Wrapper.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Wrapper(props) {
+    return <div class="lg:container lg:mx-auto" {...props} />
+}
+
+export default Wrapper;
