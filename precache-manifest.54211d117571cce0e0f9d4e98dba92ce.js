b002338599e5e986a1438d941ee47f8a4dd8878f
Strahinja Ajvaz <strahinja.ajvaz@gmail.com>
2020-08-29 23:44:54 +1000

    Updates

---
+++ precache-manifest.54211d117571cce0e0f9d4e98dba92ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1049cfef415080ea4695d9e8a41fc66",
+    "url": "/drone_data/index.html"
+  },
+  {
+    "revision": "78718071a9e6b12a3274",
+    "url": "/drone_data/static/css/main.af478c1f.chunk.css"
+  },
+  {
+    "revision": "30173902e3093bf0a084",
+    "url": "/drone_data/static/js/2.6f330893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drone_data/static/js/2.6f330893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78718071a9e6b12a3274",
+    "url": "/drone_data/static/js/main.31152ebb.chunk.js"
+  },
+  {
+    "revision": "74eac816c5dc219a8bf2",
+    "url": "/drone_data/static/js/runtime-main.510f6005.js"
+  }
+]);
